0e0c0b87079f382dceb5e67750c7688545c245b7
Michael Campbell <mcampbell0918@gmail.com>
2019-09-02 22:55:20 -0500

    inline styling

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -3,15 +3,14 @@ import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles}>
-      <View>
-        <TextInput placeholder={'Text Here'} style={{borderBottomColor: 'black', borderBottomWidth: 1}}/>
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder={'Text Here'} style={{borderColor: 'black', borderWidth: 1, padding: 10, width: '80%'}}/>
         <Button title="Add" />
+      </View>
+      <View>
+
       </View>
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  padding: 50
-});
+};
